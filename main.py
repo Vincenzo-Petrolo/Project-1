05cbcff4e0c2c2877fe19a91594355cbb80a0221
Vincenzo Petrolo <vincenzo.petrolo99@gmail.com>
2022-11-21 12:40:33 -0600

    milestone : fanout sorted experiment done

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 # Project 1 main
 
-from programs import experimentAlternate
+from programs import experimentFanout
 
 def main():  
   for test_bench in [ "seq_benches/s27.bench",
                       "seq_benches/s208.bench",
-                      "seq_benches/s208.bench",
+                      "seq_benches/s298.bench",
                       "seq_benches/s344.bench",
                       "seq_benches/s1423.bench"]:
     print(test_bench)
     for ts_len in [1, 3, 5, 7, 10]:
       # Perform experiment 3 times
       for i in range(3):
-        experimentAlternate(
+        experimentFanout(
           circuit_name=test_bench,
           n_runs=10,
           n_test_sequence=100,
           test_sequence_length=ts_len,
-          starting_value=('1','1')
+          fanout_th=3,
+          init_value=('0','0')
         )
 
 
--- programs.py
+++ programs.py
@@ -113,3 +113,40 @@ def experimentAlternate(circuit_name, n_runs, test_sequence_length, n_test_seque
   
   print(f"#Faults={n_runs}, #Detected[U]={counter_u}, #Detected[I]={counter_i}, length={test_sequence_length}, #TS={n_test_sequence}")
 
+
+def experimentFanout(circuit_name, n_runs, test_sequence_length, n_test_sequence, fanout_th, init_value):
+  # Clear the screen from previous output
+  parser = p.Parser()
+  circuit = parser.readFile(circuit_name)
+
+  # Create the sequential simulator
+  seqsim = SequentialSimulation(circuit)
+
+  __ZERO__ = ('0', '0')
+  __ONE__ = ('1', '1')
+
+  fault_list = circuit.getFullFaultList()
+
+  report_filename = "report.csv"
+  counter_u = 0
+  counter_i = 0
+
+  with open(report_filename, "w") as f:
+    f.write(f"Fault, FF_init, detected\n")
+    for i in range(n_runs):
+      ff_init_values = getFanOutFFInitValues(circuit=circuit, fanout_th=fanout_th, init_value=init_value)
+      print(len(ff_init_values.keys()))
+      # Pick a random fault
+      fault = random.choice(fault_list)
+      for j in range(n_test_sequence):
+        test_sequence = getRandomTestSequence(circuit=circuit, length=test_sequence_length)
+        seqsim.simulate(test_sequence=test_sequence, ff_init_values={}, fault=fault)
+        if (seqsim.faultDetected()):
+          counter_u += 1
+        seqsim.simulate(test_sequence=test_sequence, ff_init_values=ff_init_values, fault=fault)
+        if (seqsim.faultDetected()):
+          counter_i += 1
+        #seqsim._simulationReport()
+  
+  print(f"#Faults={n_runs}, #Detected[U]={counter_u}, #Detected[I]={counter_i}, length={test_sequence_length}, #TS={n_test_sequence}")
+
